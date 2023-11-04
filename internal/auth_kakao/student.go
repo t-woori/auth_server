ae8cdeb054fca5260359334d0285618be668c5fc
aronalee <aronalee10@gmail.com>
2023-11-04 11:22:55 +0900

    fix: json 키 변경
    - snake case로 변경

--- internal/auth_kakao/student.go
+++ internal/auth_kakao/student.go
@@ -3,8 +3,8 @@ package auth_kakao
 import "github.com/google/uuid"
 
 type Student struct {
-	StudentId    uuid.UUID
-	AccessToken  string
-	RefreshToken string
-	Nickname     string
+	StudentId    uuid.UUID `json:"student_id"`
+	AccessToken  string    `json:"access_token"`
+	RefreshToken string    `json:"refresh_token"`
+	Nickname     string    `json:"nickname"`
 }
